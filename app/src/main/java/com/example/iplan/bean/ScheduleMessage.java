9c3adabd9c4cfdf5a06f2c8394fbfb403447e189
王先生 <835779808@qq.xom>
2017-05-24 08:27:58 +0800

    新建plan对象，初步解耦。

--- app/src/main/java/com/example/iplan/bean/ScheduleMessage.java
+++ app/src/main/java/com/example/iplan/bean/ScheduleMessage.java
@@ -22,7 +22,7 @@ public class ScheduleMessage extends BmobIMExtraMessage {
     public boolean isTransient() {
         //设置为true,表明为暂态消息，那么这条消息并不会保存到本地db中，SDK只负责发送出去
         //设置为false,则会保存到指定会话的数据库中
-        return true;
+        return false;
     }
 
 
