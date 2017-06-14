21d810de7af0e387af6d980108331213991bf0b0
qinxinxin <1473020280@qq.com>
2017-06-14 20:33:55 +0800

    头像添加

---
+++ app/src/main/java/com/example/iplan/ui/MessageType.java
@@ -0,0 +1,9 @@
+package com.example.iplan.ui;
+
+/**
+ * Created by Administrator on 2017/6/14.
+ */
+
+class MessageType {
+    public static final int MINE_INFO_FINISH_FIND_USER = 0;
+}
---
+++ app/src/main/java/com/example/iplan/ui/MobclickAgent.java
@@ -0,0 +1,23 @@
+package com.example.iplan.ui;
+
+/**
+ * Created by Administrator on 2017/6/14.
+ */
+
+class MobclickAgent {
+    public static void onPageStart(String userInfoChange) {
+
+    }
+
+    public static void onResume(UserInfoChange userInfoChange) {
+
+    }
+
+    public static void onPageEnd(String userInfoChange) {
+
+    }
+
+    public static void onPause(UserInfoChange userInfoChange) {
+
+    }
+}
